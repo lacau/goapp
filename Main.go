0cca2140e764b4dd75a6a0245d5cd2fe97b4cf94
lacau <lacau@zpay.com.br>
2020-09-18 16:16:25 -0300

    Working with variable declaration, scopes and shadowing

--- Main.go
+++ Main.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+var GlobalScopevar string = "global scope variable"
+var pkgScopeVar int = 50
+
 func main() {
-	fmt.Println("Testing first go app")
+	fmt.Printf("value: %v, type: %T\n", GlobalScopevar, GlobalScopevar)
+	fmt.Printf("value: %v, type: %T\n", pkgScopeVar, pkgScopeVar)
+
+	var funcScopeVar1 float32 = 30.5
+	funcScopeVar2 := 30
+
+	fmt.Printf("value: %v, type: %T\n", funcScopeVar1, funcScopeVar1)
+	fmt.Printf("value: %v, type: %T\n", funcScopeVar2, funcScopeVar2)
+
+	GlobalScopevar = "shadowing global scope variable"
+	pkgScopeVar = 49
+
+	fmt.Println("Shadow variables")
+	fmt.Printf("value: %v, type: %T\n", GlobalScopevar, GlobalScopevar)
+	fmt.Printf("value: %v, type: %T\n", pkgScopeVar, pkgScopeVar)
 }
