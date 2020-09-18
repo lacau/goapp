1f54289114e11fb19ac86c0ed13dd565b7ba8cb9
lacau <lacau@zpay.com.br>
2020-09-18 18:48:35 -0300

    Working with constants, enumeration with iota and drop const value

--- Main.go
+++ Main.go
@@ -1,23 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	a = iota
+	b
+	c
+)
+
+const (
+	a2 = iota
+)
+
+const (
+	_ = iota
+	firstValue
+)
 
 func main() {
-	s := "This is a string"
-	fmt.Printf("%v, %T\n", s, s)
-	fmt.Printf("%v, %T\n", s[2], s[2])
-	fmt.Println("string are aliases for bytes")
-	fmt.Printf("%v, %T\n", string(s[2]), s[2])
-	fmt.Println("strings are generally immutable")
+	fmt.Println("Working with constants")
 
-	fmt.Printf("s = %v\n", s)
-	fmt.Printf("s + s = %v\n", s+s)
+	const myConst1 int = 42
+	fmt.Printf("%v, %T\n", myConst1, myConst1)
 
-	fmt.Println("string to byte slices")
-	b := []byte(s)
-	fmt.Printf("%v, %T\n", b, b)
+	const myConst2 float64 = 42.42
+	fmt.Printf("%v, %T\n", myConst2, myConst2)
 
-	fmt.Println("runes 'x', runes are a type alias for int32")
-	var r rune = 'a'
-	fmt.Printf("%v, %T\n", r, r)
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%v\n", a2)
+	fmt.Printf("%v\n", firstValue)
+
+	fmt.Println("primitive types can be constant, except arrays")
+	fmt.Println("arithmetic operations can be perform between constants and variables")
+	fmt.Println("WARNING: package and global scope constants be shadowed")
+	fmt.Println("use _ on enumerated constants to drop allocated value")
 }
