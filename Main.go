90cf69051260c9262022414de8a5b61b8a7cf7a5
lacau <lacau@zpay.com.br>
2020-09-18 18:53:24 -0300

    Using bitwise operations along with iota and bit masks to effectively store multiple data into a single byte

--- Main.go
+++ Main.go
@@ -1,41 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 const (
-	a = iota
-	b
-	c
-)
-
-const (
-	a2 = iota
-)
-
-const (
-	_ = iota
-	firstValue
+	isAdmin = 1 << iota
+	isHeadquarters
+	canSeeFinancials
 )
 
 func main() {
-	fmt.Println("Working with constants")
-
-	const myConst1 int = 42
-	fmt.Printf("%v, %T\n", myConst1, myConst1)
-
-	const myConst2 float64 = 42.42
-	fmt.Printf("%v, %T\n", myConst2, myConst2)
-
-	fmt.Printf("%v\n", a)
-	fmt.Printf("%v\n", b)
-	fmt.Printf("%v\n", c)
-	fmt.Printf("%v\n", a2)
-	fmt.Printf("%v\n", firstValue)
-
-	fmt.Println("primitive types can be constant, except arrays")
-	fmt.Println("arithmetic operations can be perform between constants and variables")
-	fmt.Println("WARNING: package and global scope constants be shadowed")
-	fmt.Println("use _ on enumerated constants to drop allocated value")
+	var roles byte = isAdmin | isHeadquarters
+	fmt.Printf("%b\n", roles)
+	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
+	fmt.Printf("Is HQ? %v\n", isHeadquarters&roles == isHeadquarters)
+	fmt.Printf("Can see financials? %v\n", canSeeFinancials&roles == canSeeFinancials)
 }
