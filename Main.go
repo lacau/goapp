987c2bd2f9b617896e1ed395a0a656496150db62
lacau <lacau@zpay.com.br>
2020-09-18 19:54:47 -0300

    Working with arrays, arrays references and copies

--- Main.go
+++ Main.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
-const (
-	isAdmin = 1 << iota
-	isHeadquarters
-	canSeeFinancials
-)
-
 func main() {
-	var roles byte = isAdmin | isHeadquarters
-	fmt.Printf("%b\n", roles)
-	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
-	fmt.Printf("Is HQ? %v\n", isHeadquarters&roles == isHeadquarters)
-	fmt.Printf("Can see financials? %v\n", canSeeFinancials&roles == canSeeFinancials)
+	grades := []int{97, 85, 93}
+	fmt.Printf("Grades: %v\n", grades)
+
+	var students [3]string
+	fmt.Printf("Students: %v\n", students)
+	students[0] = "Leandro"
+	students[1] = "Alinne"
+	students[2] = "Sophia"
+	fmt.Printf("Students: %v\n", students)
+	fmt.Printf("Number of students: %v\n", len(students))
+
+	fmt.Println("WARING: When assign an array to another variable and it was declared with [...], it is a copy!")
+
+	a := [...]int{1, 2, 3}
+	b := a
+	a[2] = 5
+	fmt.Println(a)
+	fmt.Println(b)
 }
