fd2d117ea1bd7cba218d00d14833828d7f32739b
lacau <lacau@zpay.com.br>
2020-09-19 19:16:03 -0300

    Working with slices syntax

--- Main.go
+++ Main.go
@@ -3,22 +3,49 @@ package main
 import "fmt"
 
 func main() {
-	grades := []int{97, 85, 93}
-	fmt.Printf("Grades: %v\n", grades)
-
-	var students [3]string
-	fmt.Printf("Students: %v\n", students)
-	students[0] = "Leandro"
-	students[1] = "Alinne"
-	students[2] = "Sophia"
-	fmt.Printf("Students: %v\n", students)
-	fmt.Printf("Number of students: %v\n", len(students))
-
-	fmt.Println("WARING: When assign an array to another variable and it was declared with [...], it is a copy!")
-
-	a := [...]int{1, 2, 3}
+	a := []int{1, 2, 3}
 	b := a
-	a[2] = 5
+	b[2] = 5
 	fmt.Println(a)
+	fmt.Printf("Length: %v\n", len(a))
+	fmt.Printf("Capacity: %v\n", cap(a))
 	fmt.Println(b)
+	fmt.Printf("Length: %v\n", len(b))
+	fmt.Printf("Capacity: %v\n", cap(b))
+
+	fmt.Println("Slice syntax x[y:z] - y is inclusive and z is exclusive")
+	fmt.Println("both params are optional so, x[:] is a valid syntax")
+	fmt.Println("WARNING: Slices initialize as a pointer by default")
+
+	s1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	s2 := s1[:]
+	s3 := s1[3:]
+	s4 := s1[:6]
+	s5 := s1[3:6]
+	fmt.Printf("%v\n", s1)
+	fmt.Printf("%v\n", s2)
+	fmt.Printf("%v\n", s3)
+	fmt.Printf("%v\n", s4)
+	fmt.Printf("%v\n", s5)
+
+	fmt.Println("Using make to create slices")
+	m1 := make([]int, 3, 100)
+	fmt.Println(m1)
+	fmt.Printf("Length: %v\n", len(m1))
+	fmt.Printf("Capacity: %v\n", cap(m1))
+
+	si := []int{}
+	fmt.Println(si)
+	fmt.Printf("Length: %v\n", len(si))
+	fmt.Printf("Capacity: %v\n", cap(si))
+
+	si = append(si, 1)
+	fmt.Println(si)
+	fmt.Printf("Length: %v\n", len(si))
+	fmt.Printf("Capacity: %v\n", cap(si))
+
+	si = append(a, []int{1, 2, 3, 4, 5}...)
+	fmt.Println(si)
+	fmt.Printf("Length: %v\n", len(si))
+	fmt.Printf("Capacity: %v\n", cap(si))
 }
