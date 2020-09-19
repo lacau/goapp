25b85be322d0f01ca73240540f0d0092c9bd4d40
lacau <lacau@zpay.com.br>
2020-09-19 20:18:28 -0300

    Working with maps, manipulating, ok syntax

--- Main.go
+++ Main.go
@@ -3,49 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1, 2, 3}
-	b := a
-	b[2] = 5
-	fmt.Println(a)
-	fmt.Printf("Length: %v\n", len(a))
-	fmt.Printf("Capacity: %v\n", cap(a))
-	fmt.Println(b)
-	fmt.Printf("Length: %v\n", len(b))
-	fmt.Printf("Capacity: %v\n", cap(b))
+	statePopulation := make(map[string]int)
+	statePopulation = map[string]int{
+		"California": 39250017,
+		"Texas":      27862596,
+		"Florida":    20612439,
+		"New York":   19745289,
+	}
 
-	fmt.Println("Slice syntax x[y:z] - y is inclusive and z is exclusive")
-	fmt.Println("both params are optional so, x[:] is a valid syntax")
-	fmt.Println("WARNING: Slices initialize as a pointer by default")
+	fmt.Println(statePopulation)
+	fmt.Println("PS: arrays can be map's key, but slices can't!")
+	fmt.Println("WARNING: maps are initialized as references")
+	fmt.Printf("key: Texas, value: %v\n", statePopulation["Texas"])
 
-	s1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	s2 := s1[:]
-	s3 := s1[3:]
-	s4 := s1[:6]
-	s5 := s1[3:6]
-	fmt.Printf("%v\n", s1)
-	fmt.Printf("%v\n", s2)
-	fmt.Printf("%v\n", s3)
-	fmt.Printf("%v\n", s4)
-	fmt.Printf("%v\n", s5)
+	statePopulation["Test"] = 55000
+	fmt.Println(statePopulation)
+	delete(statePopulation, "Test")
+	fmt.Println(statePopulation)
 
-	fmt.Println("Using make to create slices")
-	m1 := make([]int, 3, 100)
-	fmt.Println(m1)
-	fmt.Printf("Length: %v\n", len(m1))
-	fmt.Printf("Capacity: %v\n", cap(m1))
-
-	si := []int{}
-	fmt.Println(si)
-	fmt.Printf("Length: %v\n", len(si))
-	fmt.Printf("Capacity: %v\n", cap(si))
-
-	si = append(si, 1)
-	fmt.Println(si)
-	fmt.Printf("Length: %v\n", len(si))
-	fmt.Printf("Capacity: %v\n", cap(si))
-
-	si = append(a, []int{1, 2, 3, 4, 5}...)
-	fmt.Println(si)
-	fmt.Printf("Length: %v\n", len(si))
-	fmt.Printf("Capacity: %v\n", cap(si))
+	pop, ok := statePopulation["Test"]
+	fmt.Println(pop, ok)
+	pop, ok = statePopulation["Florida"]
+	fmt.Println(pop, ok)
 }
